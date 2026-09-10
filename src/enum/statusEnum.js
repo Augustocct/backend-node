13002eea3ddd7165070226ae55242dca0134d014
Augustocct <bokadetoka@gmail.com>
2026-09-10 17:42:32 -0300

    alteração na tabela de chamados, adicionando enum de status

---
+++ src/enum/statusEnum.js
@@ -0,0 +1,8 @@
+const StatusChamado = Object.freeze({
+    ABERTO: "ABERTO",
+    EM_ATENDIMENTO: "EM_ATENDIMENTO",
+    RESOLVIDO: "RESOLVIDO",
+    FECHADO: "FECHADO"
+});
+
+module.exports = StatusChamado;
