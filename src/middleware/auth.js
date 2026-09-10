072c567b7d0db84db8edac95e42b599a6a478036
Augustocct <bokadetoka@gmail.com>
2026-09-10 16:44:57 -0300

    comentarios em middleware de autenticacao

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,16 +4,21 @@ const blacklist = require("../service/tokenBlacklist");
 
 const authMiddleware = (req, res, next) => {
 
+
+    // PEGA O TOKEN DA SESSAO ATUAL
     const authHeader = req.headers.authorization;
 
+    // VE SE O TOKEN FOI INFORMADO
     if (!authHeader) {
         return res.status(401).json({
             message: "Token não informado"
         });
     }
 
+    // RETIRA O BEARER DO TOKEN 
     const token = authHeader.replace("Bearer ", "");
 
+    // VERIFICA SE O TOKEN FOI REVOGADO
      if (blacklist.has(token)) {
         return res.status(401).json({
             message: "Token revogado"
@@ -23,17 +28,20 @@ const authMiddleware = (req, res, next) => {
 
     try {
 
+        // VALIDA O TOKEN E RECUPERA OS DADOS DO USUÁRIO
         const decoded = jwt.verify(
             token,
             process.env.JWT_SECRET
         );
 
+        // ARMAZENA OS DADOS DO USUÁRIO NA REQUISIÇÃO
         req.user = decoded;
 
         return next();
 
     } catch (err) {
 
+        // RETORNA ERRO CASO O TOKEN SEJA INVÁLIDO OU EXPIRADO
         return res.status(401).json({
             message: err.message
         });
